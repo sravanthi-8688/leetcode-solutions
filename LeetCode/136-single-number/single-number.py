add42cea51fcd1554a1c1ae47900ae4f1b150757
sravanthi <doddisravanthi2@gmail.com>
2026-03-27 12:41:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (68.07%) - LeetSync

---
+++ LeetCode/136-single-number/single-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res=0
+        for i in nums:
+            res^=i
+        return res
+        
+          
+              
+
+
+        
+        
