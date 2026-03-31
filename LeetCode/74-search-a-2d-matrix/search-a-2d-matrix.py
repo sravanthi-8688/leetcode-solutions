62ccdd9d48c90ba0f6a3abb60b4b42ff07831310
sravanthi <doddisravanthi2@gmail.com>
2026-03-31 19:00:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (49.24%) - LeetSync

---
+++ LeetCode/74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==target:
+                    return True
+        return False
+        
