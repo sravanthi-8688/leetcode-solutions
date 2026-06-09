3685d4fa6d0134aa52f88d487aeb0853c5e41b2e
sravanthi <doddisravanthi2@gmail.com>
2026-06-09 18:26:35 +0530

    Time: 7 ms (5.23%) | Memory: 12.4 MB (53.26%) - LeetSync

---
+++ LeetCode/258-add-digits/add-digits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addDigits(self, num):
+        while num >= 10:
+            s = 0
+            while num > 0:
+                s += num % 10
+                num //= 10
+            num = s
+        return num
