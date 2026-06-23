c7089ce1ba927aaffbf4b6e31357778f477708c5
sravanthi <doddisravanthi2@gmail.com>
2026-06-23 17:27:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (61.55%) - LeetSync

--- LeetCode/1791-richest-customer-wealth/richest-customer-wealth.py
+++ LeetCode/1791-richest-customer-wealth/richest-customer-wealth.py
@@ -4,10 +4,7 @@ class Solution(object):
         :type accounts: List[List[int]]
         :rtype: int
         """
-        max_wealth=0
+        rich=0
         for i in accounts:
-            curr_sum=0
-            for j in i:
-                curr_sum+=j
-                max_wealth=max(curr_sum,max_wealth)
-        return max_wealth
+            rich=max(rich,sum(i))
+        return rich
