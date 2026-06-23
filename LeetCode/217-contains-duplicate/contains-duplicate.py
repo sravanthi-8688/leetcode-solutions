2a6b8fe8a20f472056705a6c78c4024c782f68e7
sravanthi <doddisravanthi2@gmail.com>
2026-06-23 18:24:11 +0530

    Time: 18 ms (64.03%) | Memory: 26 MB (28.38%) - LeetSync

--- LeetCode/217-contains-duplicate/contains-duplicate.py
+++ LeetCode/217-contains-duplicate/contains-duplicate.py
@@ -4,9 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        s=set()
-        for i in nums:
-            if i in s:
-                return True
-            s.add(i)
-        return False
+        return len(nums)!=len(set(nums))
