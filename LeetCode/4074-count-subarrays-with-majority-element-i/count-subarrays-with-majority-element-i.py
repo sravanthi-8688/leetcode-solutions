b8c1cfd43cc20f982ffe11624e1b0dee2ec3a374
sravanthi <doddisravanthi2@gmail.com>
2026-06-25 21:20:19 +0530

    Time: 381 ms (67.44%) | Memory: 12.6 MB (41.86%) - LeetSync

--- LeetCode/4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.py
+++ LeetCode/4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.py
@@ -6,21 +6,16 @@ class Solution(object):
         :rtype: int
         """
         prefix = 0
-        count = 0
-        
+        count = 0    
         freq = {0: 1}
-        
         for num in nums:
             if num == target:
                 prefix += 1
             else:
                 prefix -= 1
-            
             for k in freq:
                 if k < prefix:
                     count += freq[k]
-            
             freq[prefix] = freq.get(prefix, 0) + 1
-        
         return count
         
