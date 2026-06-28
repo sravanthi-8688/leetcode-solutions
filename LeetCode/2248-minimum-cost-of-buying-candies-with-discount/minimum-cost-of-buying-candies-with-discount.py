f8348e43777402f3d26df824856f12015fecaeee
sravanthi <doddisravanthi2@gmail.com>
2026-06-28 11:47:00 +0530

    Time: 4 ms (18.73%) | Memory: 12.3 MB (90.30%) - LeetSync

---
+++ LeetCode/2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minimumCost(self, cost):
+        cost.sort(reverse=True)
+        total = 0
+        for i in range(len(cost)):
+            if i % 3 != 2:
+                total += cost[i]
+        return total
