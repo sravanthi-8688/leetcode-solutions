62d8d23786adcd61b890dc52d58fc80858d0c598
sravanthi <doddisravanthi2@gmail.com>
2026-06-29 17:18:41 +0530

    Time: 56 ms (35.00%) | Memory: 22.3 MB (33.14%) - LeetSync

---
+++ LeetCode/3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def processStr(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = []
+        n = len(s)
+
+        for i in range(n):
+            ch = s[i]
+
+            if ch == '*':
+                if len(res) != 0:
+                    res.pop()
+            elif ch == '#':
+                res.extend(res)
+            elif ch == '%':
+                res.reverse()
+            elif 'a' <= ch <= 'z':
+                res.append(ch)
+
+        return ''.join(res)
+        
