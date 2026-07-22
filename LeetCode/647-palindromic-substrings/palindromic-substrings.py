21f13e657104d1c1cefdb542c5f31317efc5b0c3
sravanthi <doddisravanthi2@gmail.com>
2026-07-22 10:37:52 +0530

    Time: 1300 ms (6.36%) | Memory: 12.4 MB (38.55%) - LeetSync

---
+++ LeetCode/647-palindromic-substrings/palindromic-substrings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count=0
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                if s[i:j+1]==s[i:j+1][::-1]:
+                    count+=1
+        return count
+        
