74709867864bae5a19d62b6102a689db374ea638
sravanthi <doddisravanthi2@gmail.com>
2026-07-25 20:10:32 +0530

    Time: 4 ms (34.97%) | Memory: 12.3 MB (86.01%) - LeetSync

---
+++ LeetCode/3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxProduct(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        n1=str(n)
+        l=n1.strip()
+        l2=[]
+        for i in l:
+            l2.append(int(i))
+        l2.sort()
+        return l2[-1]*l2[-2]
+      
+        
