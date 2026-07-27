4656a9baa7eef6f04b9f9e1a500292b48b087328
sravanthi <doddisravanthi2@gmail.com>
2026-07-27 19:16:08 +0530

    Time: 4 ms (28.09%) | Memory: 12.5 MB (37.88%) - LeetSync

--- LeetCode/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
+++ LeetCode/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
@@ -4,12 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n=len(nums)
-        ms=0
-        for i in range(n):
-            for j in range(i+1,n):
-                if (nums[i]-1)*(nums[j]-1)>ms:
-                    ms=(nums[i]-1)*(nums[j]-1)
-        return ms
+        nums.sort()
+        return (nums[-1]-1)*(nums[-2]-1)
 
         
