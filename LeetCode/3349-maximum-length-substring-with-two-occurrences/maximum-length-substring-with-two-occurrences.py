a045a20bcd79030336a9d64208826b3c270fe80c
sravanthi <doddisravanthi2@gmail.com>
2026-08-14 18:39:30 +0530

    Time: 2 ms (92.14%) | Memory: 12.4 MB (22.86%) - LeetSync

--- LeetCode/3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
+++ LeetCode/3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        count = {}
-        i = res = 0
-        for j, c in enumerate(s):
-            count[c] = count.get(c, 0) + 1
-            while count[c] > 2:
-                count[s[i]] -= 1
-                i += 1
-            res = max(res, j - i + 1)
+        count={}
+        i=res=0
+        for j,c in enumerate(s):
+            count[c]=count.get(c,0)+1
+            while count[c]>2:
+                count[s[i]]-=1
+                i+=1
+            res=max(res,j-i+1)
         return res
         
