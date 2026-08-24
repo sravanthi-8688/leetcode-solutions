86d81ca3c3a14e3cd4d8ea7b425d4226cd851ad5
sravanthi <doddisravanthi2@gmail.com>
2026-08-24 20:38:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (16.69%) - LeetSync

---
+++ LeetCode/782-jewels-and-stones/jewels-and-stones.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        c=0
+        for i in stones:
+            if i in jewels:
+                c+=1
+        return c
+        
