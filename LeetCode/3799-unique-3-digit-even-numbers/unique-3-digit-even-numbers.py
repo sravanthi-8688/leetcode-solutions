57671c69cb98a67a537afb82266b924b18da9afa
sravanthi <doddisravanthi2@gmail.com>
2026-09-11 18:58:17 +0530

    Time: 12 ms (95.24%) | Memory: 12.4 MB (26.19%) - LeetSync

---
+++ LeetCode/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def totalNumbers(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: int
+        """
+        numbers=set()
+        for a, b, c in permutations(digits,3):
+            if a!=0 and c%2==0:
+                numbers.add((a, b, c))
+        return len(numbers)
+        
